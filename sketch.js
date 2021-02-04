168e6ce7d0d29f6303426b2958abcf012a808a72
afrnmohsin <76119725+afrnmohsin@users.noreply.github.com>
2021-02-04 08:56:19 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var hball,database,position;
+
+function setup(){
+    database = firebase.database;
+    console.log(database);
+    createCanvas(500,500);
+    hball = createSprite(250,250,10,10);
+    hball.shapeColor = "red";
+
+    var hballposition = database.ref('ball/position');
+    hballposition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+
+    'x' : position.x + x,
+    'y' : position.y + y
+});
+}
+
+function readPosition(){
+    position = data.val();
+    console.log(position.x);
+    hball.x = position.x;
+    hball.y = position.y;
+
+}
